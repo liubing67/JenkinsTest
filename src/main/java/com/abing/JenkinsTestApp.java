f6d93aa9f54183a87b7a9b500d9e2f5e987788d8
liubing <724266839@qq.com>
2019-05-09 11:37:50 +0800

    Initial commit

---
+++ src/main/java/com/abing/JenkinsTestApp.java
@@ -0,0 +1,11 @@
+package com.abing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApp.class, args);
+    }
+}
